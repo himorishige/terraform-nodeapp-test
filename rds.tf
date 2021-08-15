3ed8505d713df199e1c040c523080e0a66db6ede
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-15 14:45:15 +0900

    add rds

--- rds.tf
+++ rds.tf
@@ -28,7 +28,7 @@ resource "aws_db_option_group" "mysql_standalone_optiongroup" {
 # ----
 # RDS subnet group
 # ----
-resource "aws_db_subnet_group" "mysql-standalone_subnetgroup" {
+resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
   name = "${var.project}-${var.environment}-mysql-standalone-subnetgroup"
   subnet_ids = [
     aws_subnet.private_subnet_1a.id,
@@ -37,6 +37,58 @@ resource "aws_db_subnet_group" "mysql-standalone_subnetgroup" {
 
   tags = {
     Name    = "${var.project}-${var.environment}-mysql-standalone-subnetgroup"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# ----
+# RDS instance
+# ----
+resource "random_string" "db_password" {
+  length  = 16
+  special = false
+}
+
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.20"
+
+  identifier = "${var.project}-${var.environment}-mysql-standalone"
+
+  username = "admin"
+  password = random_string.db_password.result
+
+  instance_class = "db.t2.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+  backup_window              = "04:00-05:00"
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:00-Mon:06:00"
+  auto_minor_version_upgrade = false
+
+  deletion_protection = false
+  skip_final_snapshot = true
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone"
     Project = var.project
     Env     = var.environment
   }

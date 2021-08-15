9674f0096d9ae956b931fb8e8d0a08aa31f7504f
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-15 18:28:35 +0900

    add: sms

--- appserver.tf
+++ appserver.tf
@@ -13,6 +13,39 @@ resource "aws_key_pair" "keypair" {
 }
 
 # ----
+# SSM Parameter Store
+# ----
+resource "aws_ssm_parameter" "host" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.port
+}
+
+resource "aws_ssm_parameter" "database" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.password
+}
+
+# ----
 # EC2 Instance
 # ----
 resource "aws_instance" "app_server" {

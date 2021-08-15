617505d9c7795b3aa5c0b67984c4416a7dd9a485
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-15 16:15:43 +0900

    add ec2

---
+++ appserver.tf
@@ -0,0 +1,35 @@
+# ----
+# Key pair
+# ----
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.project}-${var.environment}-keypair"
+  public_key = file("./src/tastylog-dev-keypair.pub")
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-keypair"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# ----
+# EC2 Instance
+# ----
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id
+  ]
+  key_name = aws_key_pair.keypair.key_name
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-ec2"
+    Project = var.project
+    Env     = var.environment
+    Type    = "app"
+  }
+}
--- data.tf
+++ data.tf
@@ -1,3 +1,23 @@
 data "aws_prefix_list" "s3_pl" {
   name = "com.amazonaws.*.s3"
 }
+
+data "aws_ami" "app" {
+  most_recent = true
+  owners      = ["self", "amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}

c2cbc98e48f5ed87094fb5f97cec6929d943e803
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-15 17:11:08 +0900

    add s3

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "s3" {
+    bucket  = "tastylog-tfstate-bucket-himorishige"
+    key     = "tastylog-dev.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
 }
 
 # ----

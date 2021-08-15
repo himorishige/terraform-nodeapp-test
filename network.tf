342f99b413fda7dae41c487a70b13d9a60767b5c
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-15 12:51:27 +0900

    add igw

--- network.tf
+++ network.tf
@@ -118,3 +118,22 @@ resource "aws_route_table_association" "private_rt_1c" {
   route_table_id = aws_route_table.private_rt.id
   subnet_id      = aws_subnet.private_subnet_1c.id
 }
+
+# ----
+# Internet Gateway
+# ----
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_route" "public_rt_igw_r" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
